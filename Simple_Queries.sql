52a47d9d45040cc92791e9182597f2ed7184875c
Dreday <54422312+Dreday904@users.noreply.github.com>
2021-11-23 11:59:32 -0500

    Simple solutions to queries.

---
+++ Simple_Queries.sql
@@ -0,0 +1,43 @@
+#1
+SELECT productName AS 'Name', productLine AS 'Product Line', buyPrice AS 'Buy Price'
+FROM products
+ORDER BY buyPrice
+DESC;
+
+
+#2
+SELECT contactFirstName AS 'First Name', contactLastName AS 'Last Name', city AS 'City', country 
+FROM customers
+WHERE country='germany'
+ORDER BY contactLastName
+ASC; 
+
+
+#3
+SELECT UNIQUE status
+FROM orders
+ORDER BY status
+asc; 
+
+
+#4
+SELECT * 
+FROM payments
+WHERE paymentDate >= '2005-1-1'
+ORDER BY paymentDate
+ASC;
+
+#5
+#SELECT lastName, firstName, email, jobTitle, city
+#FROM employees, offices
+#WHERE city = 'San Francisco'
+#ORDER BY lastName
+#ASC;
+
+
+#6
+SELECT productName, productLine, productScale, productVendor
+FROM products
+WHERE productLine IN ('Vintage Cars','Classic Cars')
+ORDER by productName
+asc;
